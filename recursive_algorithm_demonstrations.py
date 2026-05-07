7d456ec3251a8ae14a4ba60a267cae35c45d120e
Iain Gunter-Smith <iain.gunter-smith@sd70.bc.ca>
2026-05-07 15:01:45 -0700

    Create recursive_algorithm_demonstrations.py
    Co-authored-by: athyfr <athyfr@proton.me>

---
+++ recursive_algorithm_demonstrations.py
@@ -0,0 +1,33 @@
+"""Recursive algorithms.
+
+A collection of recursive algorithms created to satisfy the curricular
+requirements of the BC Computer Science 12 course.
+
+Algorithms:
+- Fibonacci sequence
+- Exponentiation (repeated multiplication)
+- Factorials
+- Palindromes
+- Combinations
+- Greatest common factor
+"""
+
+class Fibonacci:
+    """Class that represents the fibbonacci sequence."""
+    _sequence: list[int] = []
+
+    def __getitem__(n: int) -> int:
+        return 1
+
+def Exponentiation(a, x):
+    """Function that returns a^x """
+    pass
+
+def Factorial(n):
+    """Function that returns the factorial of n"""
+    pass
+
+if __name__ == '__main__':
+    # The script is being run directly
+    
+pass
