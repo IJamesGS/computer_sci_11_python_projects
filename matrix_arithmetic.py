b215f39622d965ef0608cd35a3e971e54a3ae5e7
athyfr <athyfr@proton.me>
2026-05-12 14:51:12 -0700

    refactor: Correct `matrix_arithmetic.py` to follow conventions.

--- matrix_arithmetic.py
+++ matrix_arithmetic.py
@@ -1,295 +1,238 @@
-from matrix_solver_v2 import print_matrix, elimination
+from matrix_solver_v2 import elimination
 
-matrix_33A = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
+Matrix: type = list[list[float]]
+Vector: type = list[float]
 
-matrix_33B = [[4, 2, 0],
-              [6, 2, 1],
-              [6, 7, 9]]
+def get_dimensions(matrix: Matrix):
+    """Get the number of rows and columns in a matrix."""
+    rows = len(matrix)
+    cols = len(matrix[0])
+    return rows, cols
 
-matrix_23 = [[1, 3, 5],
-             [7, 9, 8]]
 
-matrix_32 = [[6, 7],
-             [6, 9],
-             [4, 2]]
+def same_dimensions(A: Matrix, B:Matrix) -> bool:
+    """Check if two matrices have the same number of columns and rows."""
+    rows_A, cols_A = get_dimensions(A)
+    rows_B, cols_B = get_dimensions(B)
 
-# returns # of rows, cols of matrix
-def get_dimensions(matrix):
-    try:
-        rows = len(matrix)
-        cols = len(matrix[0])
-        return rows, cols
+    return rows_A == rows_B and cols_A == cols_B
+
+
+def add_matricies(A: Matrix, B: Matrix, Acoeff=1, Bcoeff=1) -> Matrix:
+    """Add two matrices together."""
+    output: Matrix = []
+
+    # check if the matrices have the same dimensions
+    if same_dimensions(A, B) == False:
+        raise ValueError("Matrices have mismatched dimensions.")
+
+    rows, cols = get_dimensions(A)
+
+    # create new matrix "C"
+    for row in range(rows):
+        output.append([])
+        for _ in range(cols):
+            output[row].append(0)
+
+    # adds all the values of matrix A times a coefficient to C
+    for row in range(rows):
+        for col in range(cols):
+            output[row][col] = (A[row][col]) * Acoeff
+    # same as above, but with matrix B
+    for row in range(rows):
+        for col in range(cols):
+            output[row][col] = output[row][col] + ((B[row][col]) * Bcoeff)
+
+    return output
+
+
+def get_row_vectors(
+    in_matrix: Matrix
+) -> list[Vector]:
+    """Get a list of the row and/or column vectors of the matrix."""
+    output = []
+
+    for row in range(len(in_matrix)):
+        output.append(in_matrix[row])
+
+    return output
+
+
+def get_column_vectors(
+    in_matrix: Matrix
+) -> list[Vector]:
+    output = []
     
-    except Exception as e:
-        return "error"
-        print(f"Something went wrong: {e}")
+    for col in range(len(in_matrix[0])):
+        temp_col_vect = []
+        for row in range(len(in_matrix)):
+            temp_col_vect.append(in_matrix[row][col])
 
-# takes in two matricies, and checks if they have the same number of columns and rows
-def same_dimensions(A, B):
-    try:
-        rows_A, cols_A = get_dimensions(A)
-        rows_B, cols_B = get_dimensions(B)
-        
-        if rows_A == rows_B and cols_A == cols_B:
-            return True
-        
-        else:
-            return False
-    
-    except Exception as e:
-        return "error"
-        print(f"Something went wrong: {e}")
+        output.append(temp_col_vect)
 
-# take in matricies A & B, adds them, returns a new matrix C
-def add_matricies(A, B, Acoeff=1, Bcoeff=1):
-    try:
-        C: list[list[float]] = list()
-        
-        # check if the matricies have the same dimensions
-        if same_dimensions(A, B) == False:
-            return "error"
-        
-        rows, cols = get_dimensions(A)
-        
-        # create new matrix "C"
-        for row in range(rows):
-            C.append([])
-            for col in range(cols):
-                C[row].append(0)
-        
-        # adds all the values of matrix A times a coeffecient to C
-        for row in range(rows):
-            for col in range(cols):
-                C[row][col] = (A[row][col]) * Acoeff
-        # same as above, but with matrix B
-        for row in range(rows):
-            for col in range(cols):
-                C[row][col] = C[row][col] + ((B[row][col]) * Bcoeff)
-        
-        return C
-    
-    except Exception as e:
-        return "error"
-        print(f"Something went wrong: {e}")
-        
-# returns a list of the row and/or column vectors of matrix A
-def get_vectors(A, which_vector="both"):
-    try:
-        row_vectors = list()
-        col_vectors = list()
-        
-        if which_vector == "row":
-            for row in range(len(A)):
-                row_vectors.append(A[row])
-                
-            return row_vectors
-                
-        elif which_vector == "column":
-            for col in range(len(A[0])):
-                temp_col_vect = list()
-                for row in range(len(A)):
-                    temp_col_vect.append(A[row][col])
-                
-                col_vectors.append(temp_col_vect)
-        
-            return col_vectors
-        
-        elif which_vector == "both":
-            for col in range(len(A[0])):
-                temp_col_vect = list()
-                for row in range(len(A)):
-                    temp_col_vect.append(A[row][col])
-                
-                col_vectors.append(temp_col_vect)
-            
-            for row in range(len(A)):
-                row_vectors.append(A[row])
-                
-            return col_vectors, row_vectors
-    
-    except Exception as e:
-        return f"error: {e}"
-        print(f"Something went wrong: {e}")
-    
-# returns the product of matrix A & B in a matrix C
-def multiply_matricies(A, B):
-    try:
-        A_vectors = get_vectors(A, 'row')
-        B_vectors = get_vectors(B, 'column')
-        C: list[list[float]] = list()
-        
-        if len(A_vectors[0]) != len(B_vectors[0]):
-            return 'error'
-        
-        for row in range(len(A_vectors)):
-            C.append(list())
-            for col in range(len(B_vectors)):
-                #set up "val" for the final assigment of variable
-                val = 0
-                #loop through the vectors
-                for n in range(len(A_vectors[0])):
-                    c = 0
-                    #multiply corrosponding values between the vectors as c
-                    c = (A_vectors[row][n] * B_vectors[col][n])
-                    #add c to the value, c is then reset on the loop
-                    val += c
-                #val becomes the entry in the C matrix
-                C[row].append(val)
-        
-        return C
-    
-    except Exception as e:
-        return "other_error"
-        print(f"Something went wrong: {e}")
+    return output
 
-# creates the augmented matrix [A|B]
-def augment_matricies(A, B):
-    try:
-        C: list[list[float]] = list()
-        for row1 in range(len(A)):
-            C.append([])
-            for column1 in range(len(A[0])):
-                C[row1].append(A[row1][column1])
-        for row in range(len(B)):
-            for column in range(len(B[0])):
-                C[row].append(B[row][column])
-                
-        return C
-    
-    except Exception as e:
-        return "error"
-        print(f"Something went wrong: {e}")
 
-# returns the X matrix which solves the equation AX = B
-def solve_AX_to_B(A, B):
-    try:
-        X: list[list[float]] = list()
-            
-        cols_A = len(A[0]) - 1
-        AB = augment_matricies(A, B)
-        #runs the augmented matrix througn gaussian elimination
-        new_AB = elimination(AB, "nopivots")
-        #new_AB is now in the form [I|X]
-        #"unaugments" the matrix to aquire X
-        for row in range(len(new_AB)):
-            X.append([])
-            for column in range(len(new_AB[0])):
-                if column > cols_A:
-                    X[row].append(new_AB[row][column])
-            
-        return X
-    
-    except Exception as e:
-        return f"Something went wrong: {e}"
-        print(f"Something went wrong: {e}")
+def multiply_matricies(A: Matrix, B: Matrix) -> Matrix:
+    """Get the product of A * B as a matrix."""
+    A_vectors = get_row_vectors(A)
+    B_vectors = get_column_vectors(B)
+    output: Matrix = []
 
-# returns the transposed matrix A as B
-def matrix_transpose(A):
-    try:
-        B: list[list[float]] = list()
+    if len(A_vectors[0]) != len(B_vectors[0]):
+        raise ValueError("Matrices have mismatched dimensions.")
 
-        for column in range(len(A[0])):
-            B.append([])
-            for row in range(len(A)):
-                B[column].append(A[row][column])
-                
-        return B
-    
-    except Exception as e:
-        return "error"
-        print(f"Something went wrong: {e}")
+    for row in range(len(A_vectors)):
+        output.append([])
+        for col in range(len(B_vectors)):
+            # set up "val" for the final assignment of variable
+            val = 0
+            # loop through the vectors
+            for n in range(len(A_vectors[0])):
+                # multiply corresponding values between the vectors as c
+                c = A_vectors[row][n] * B_vectors[col][n]
+                # add c to the value, c is then reset on the loop
+                val += c
+            # val becomes the entry in the C matrix
+            output[row].append(val)
 
-# returns a new nxn identity matrix A
-def create_identity(n):
-    if n < 1:
-        return 'error'
-    
-    A: list[list[float]] = list()
-    
-    for row in range(n):
-        A.append(list())
-        for column in range(n):
+    return output
+
+
+def augment_matricies(A: Matrix, B: Matrix) -> Matrix:
+    """Create the augmented matrix [A|B]."""
+    output: Matrix = []
+
+    for row1 in range(len(A)):
+        output.append([])
+        for column1 in range(len(A[0])):
+            output[row1].append(A[row1][column1])
+
+    for row in range(len(B)):
+        for column in range(len(B[0])):
+            output[row].append(B[row][column])
+
+    return output
+
+
+def solve_XA_to_B(A: Matrix, B :Matrix) -> Matrix:
+    """Get the X matrix which solves the equation X*A = B."""
+    X: Matrix = []
+
+    cols_A = len(A[0]) - 1
+    AB = augment_matricies(A, B)
+    # runs the augmented matrix through Gaussian Elimination
+    new_AB = elimination(AB, "nopivots")
+    # new_AB is now in the form [I|X]
+    # "unaugments" the matrix to acquire X
+    for row in range(len(new_AB)):
+        X.append([])
+        for column in range(len(new_AB[0])):
+            if column > cols_A:
+                X[row].append(new_AB[row][column])
+
+    return X
+
+
+def matrix_transpose(A: Matrix) -> Matrix:
+    """Transpose a matrix."""
+    output: Matrix = []
+
+    for column in range(len(A[0])):
+        output.append([])
+        for row in range(len(A)):
+            output[column].append(A[row][column])
+
+    return output
+
+
+def create_identity(size: int) -> Matrix:
+    """Creates a new `size`x`size` identity matrix"""
+    if size < 1:
+        raise ValueError("Matrix must have dimensions greater than 0.")
+
+    output: Matrix = []
+
+    for row in range(size):
+        output.append([])
+        for column in range(size):
             if row == column:
-                A[row].append(1)
+                output[row].append(1)
             else:
-                A[row].append(0)
-                
-    return A
+                output[row].append(0)
 
-# returns matrix X which solves AX = I
-def matrix_inverse(A):
-    
-    #check square
-    Ar, Ac = get_dimensions(A)
-    if Ar != Ac:
-        return 'error'
-    
-    identity = create_identity(len(A))
-    
-    X = solve_AX_to_B(A, identity)
-    
-    #check if X is actually the inverse of A
-    check = multiply_matricies(A, X)
-    
-    if check == identity:
-        return X
-    
+    return output
+
+def matrix_inverse(in_matrix: Matrix) -> Matrix:
+    """Get the inverse of a matrix"""
+    # check square
+    size_y, size_x = get_dimensions(in_matrix)
+    if size_y != size_x:
+        raise ValueError("Matrix is not square")
+
+    identity = create_identity(len(in_matrix))
+
+    output: Matrix = solve_XA_to_B(in_matrix, identity)
+
+    # check if X is actually the inverse of A
+    check = multiply_matricies(in_matrix, output)
+
     if check != identity:
-        return 'matrix uninvertible'
+        raise ValueError("Matrix cannot be inverted.")
 
-# returns the trace of a square matrix A
-def trace(A):
+    return output
+
+
+def trace(in_matrix: Matrix) -> float:
+    """Get the trace of a square matrix"""
     t: float = 0
-    
-    #check square
-    Ar, Ac = get_dimensions(A)
-    if Ar != Ac:
-        return 'error'
-    
-    for row in range(len(A)):
-        for column in range(len(A[0])):
+
+    # check square
+    num_rows, num_columns = get_dimensions(in_matrix)
+    if num_rows != num_columns:
+        raise ValueError("Matrix is not square")
+
+    for row in range(len(in_matrix)):
+        for column in range(len(in_matrix[0])):
             if row == column:
-                t += A[row][column]
-                
+                t += in_matrix[row][column]
+
     return t
 
+
 # returns the determinate of a square matrix A that is less than 2 in dimensions
-def determinate_2_and_small(A):
+def determinate_2_and_small(in_matrix: Matrix) -> float:
+    """Get the determinate of a square matrix which is no greater than two entries in either dimension"""
     d: float = 0
-    
-    #check square
-    Ar, Ac = get_dimensions(A)
-    if Ar != Ac:
-        return 'error'
-    
-    An = Ar
-    if An == 1:
-        d = A[0][0]
-    
-    elif An == 2:
-        d = ((A[0][0]) *  (A[1][1])) - ((A[0][1]) * (A[1][0]))
-        
-    else:
-        return 'too big!'
-    
+
+    # check square
+    num_rows, num_columns = get_dimensions(in_matrix)
+    if num_rows != num_columns or num_rows > 2 or num_rows < 1:
+        raise ValueError("Matrix must have dimensions 1x1 or 2x2.")
+
+    if num_rows == 1:
+        d = in_matrix[0][0]
+
+    elif num_rows == 2:
+        d = ((in_matrix[0][0]) * (in_matrix[1][1])) - ((in_matrix[0][1]) * (in_matrix[1][0]))
+
     return d
-    
-#returns the product of Ab = c
-def mult_vector(A, b):
-    c: list[float] = list()
-    
-    if len(A[0]) != len(b):
-        return "error"
-    
-    for row in range(len(A)):
+
+
+# returns the product of Ab = c
+def mult_vector(in_matrix: Matrix, b: Vector) -> Vector:
+    """Get the product of a matrix and vector multiplication"""
+    output: Vector = []
+
+    if len(in_matrix[0]) != len(b):
+        raise ValueError("Matrices have mismatched dimensions.")
+
+    for row in range(len(in_matrix)):
         val = 0
-        for column in range(len(A[0])):
-            x = A[row][column] * b[column]
+        for column in range(len(in_matrix[0])):
+            x = in_matrix[row][column] * b[column]
             val += x
-            
-        c.append(val)
-    
-    
-    return c
+
+        output.append(val)
+
+    return output
