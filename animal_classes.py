7d89f7f68a868db5c07bb1f8464e638a5c3cddb6
Iain Gunter-Smith <iain.gunter-smith@sd70.bc.ca>
2026-06-01 14:23:35 -0700

    feat: changed code to reflect new scope of animal_classes
    Signed-off-by: Iain Gunter-Smith <iain.gunter-smith@sd70.bc.ca>

--- animal_classes.py
+++ animal_classes.py
@@ -2,56 +2,95 @@
 
 import random
 
+# example gene: AA - AAAA - AA - AA
+#               type-color-height-weight
+#               Dog - White - short(1) - light(5)
+animal_types = {"AA": "Dog", "BB": "Cat", "AB": "Dog-Cat", "BA": "Cat-Dog"}
+
+animal_colors = {
+    "AAAA": "White",
+    "AAAB": "Pink",
+    "AABA": "Red",
+    "AABB": "Peach",
+    "ABAA": "Orange",
+    "ABAB": "Lemon",
+    "ABBB": "Yellow",
+    "BAAA": "Lime",
+    "BAAB": "Green",
+    "BABA": "Cyan",
+    "BABB": "Blue",
+    "BBAA": "Indigo",
+    "BBAB": "Purple",
+    "BBBB": "Black",
+}
+
+animal_heights = {"AA": 1, "AB": 2, "BA": 3, "BB": 4}
+
+animal_weights = {"AA": 5, "AB": 10, "BA": 15, "BB": 20}
+
 
 class Animal:
-    def __init__(self, looking: bool, height: int):
+    def __init__(self, looking: bool, genetics: str):
         self.looking = looking
-        self.height = height
+        self.genetics = genetics
 
-    def multiply(self, other):
-        return Animal(False, (self.height * other.height) / 2 + random.uniform(-1, 1))
-
-    def set_looking(self, looking: bool) -> bool:
+    def set_looking(self, look: bool) -> None | bool:
         old_looking = self.looking
-
-        self.looking = looking
-
+        self.looking = look
         return old_looking
 
+    def multiply(self, other):
+        new_genetics = ""
 
-class Carnivore(Animal):
-    def __init__(self, a, b, has_eaten: bool):
-        super.__init__(a, b)
-        self.has_eaten = has_eaten
+        for i in range(len(self.genetics)):
+            inheritance = random.randint(1, 2)
+            if inheritance == 1:
+                new_genetics += self.genetics[i]
+            else:
+                new_genetics += other.genetics[i]
 
-    def eat(self, other):
-        del other
-        self.has_eaten = True
+        return Animal(False, new_genetics)
 
+    def read_genetics(self):
+        type_gene = ""
+        color_gene = ""
+        height_gene = ""
+        weight_gene = ""
 
-class Herbivore(Animal):
-    def __init__(self, a, b):
-        super.__init__(a, b)
+        for index, gene in enumerate(self.genetics):
+            if index <= 1:
+                type_gene += gene
+
+            elif index <= 5 and index >= 2:
+                color_gene += gene
+
+            elif index <= 7 and index >= 6:
+                height_gene += gene
+
+            elif index <= 9 and index >= 8:
+                weight_gene += gene
+
+        self.type = animal_types[type_gene]
+        self.color = animal_colors[color_gene]
+        self.height = animal_heights[height_gene]
+        self.weight = animal_weights[weight_gene]
 
 
 if __name__ == "__main__":
     import time
 
-    tick_time = 1 / 20
+    tick_time = 1 / 5
     last_tick = time.time()
 
     animals: list[Animal] = []
 
-    for i in range(6):
-        animals.append(Carnivore(False, random.uniform(100, 200), False))
-
-    for i in range(12):
-        animals.append(Herbivore(False, random.uniform(100, 200)))
-
     sim_length = float(input("Enter run time (seconds): "))
     start_time = time.time()
+    cycle = 0
 
     while True:
+        cycle += 1
+        print(f"cycle: {cycle}")
         extra_time = (last_tick + tick_time) - time.time()
         if extra_time > 0:
             time.sleep(extra_time)
@@ -62,12 +101,14 @@ if __name__ == "__main__":
 
         for i, animal in enumerate(animals):
             rand_val = random.randint(1, 99)
-            if rand_val > 95:
+            if rand_val > 80:
                 animal.set_looking(True)
-
                 for j, other in enumerate(animals):
-                    if other.looking and j != i and type(animal) == type(other):
-                        animal.multiply(other)
+                    if other.looking:
+                        new_animal = animal.multiply(other)
+                        new_animal.read_genetics()
+                        animals.append(new_animal)
+                        print(animals)
 
             if rand_val == 1:
                 del animal
